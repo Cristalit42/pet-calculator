63dc15a4870caf2b2e6dc96ff3d53fc67c7cf864
Daniil <cristalit42@gmail.com>
2024-08-30 23:26:03 +0600

    Рабочий прототип

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 'use strict'
-
 let displayNumber = '',
-  displaySymbol;
+  mathOperator;
 
 
 function dispalyUpdate() {
-
   if (displayNumber) {
     document.getElementById('display').innerHTML = displayNumber;
   } else {
@@ -18,38 +16,93 @@ function readNumber() {
 
   nums.forEach(num => {
     num.addEventListener('click', function (event) {
-      event.preventDefault;
+      event.preventDefault();
       displayNumber += num.textContent.trim();
       dispalyUpdate();
     })
   })
 }
 
-function clearDisplay() {
+function clearDisplayOnClick() {
   let clearElem = document.getElementById('clear__btn')
 
   clearElem.addEventListener('click', function (event) {
-    displayNumber = '';
-    dispalyUpdate();
-  })
-}
-
-function result() {
-  let resultElem = document.getElementById('result__btn');
-
-  resultElem.addEventListener('click', function (event) {
     event.preventDefault();
-    let displayInfo = document.getElementById('display').textContent;
-    displayNumber = '';
-    console.log(displayNumber, displayInfo);
-    displayNumber = eval(displayInfo);
+    value.length = 0;
+    clearDisplay();
+  })
+}
+
+function clearDisplay() {
+  displayNumber = '';
+  dispalyUpdate();
+}
+
+let value = [];
+
+function saveValue() {
+  let operators = document.querySelectorAll('.math-operator');
+
+  operators.forEach(operator => {
+    operator.addEventListener('click', function (event) {
+      event.preventDefault();
+      saveOperator(operator);
+      value.push(displayNumber.slice(0, -1));
+      console.log(value);
+      clearDisplay();
+    })
+  })
+}
+
+function saveOperator(operator) {
+  if (operator.textContent.includes("+") || operator.textContent.includes("-") || operator.textContent.includes("*") || operator.textContent.includes("/")) {
+    mathOperator = operator.textContent.trim();
+    console.log(mathOperator);
+  }
+}
+
+function solution() {
+  let a = value.shift(),
+    b = value.shift(),
+    result;
+
+  if (mathOperator == '+') {
+    return result = +a + +b;
+    value.length = 0;
+  } else if (mathOperator == '-') {
+    return result = a - b;
+    value.length = 0;
+  } else if (mathOperator == '/') {
+    return result = a / b;
+    value.length = 0;
+  } else if (mathOperator == '*') {
+    return result = a * b;
+    value.length = 0;
+  }
+}
+
+function resultOutput() {
+  let resultBtn = document.getElementById('result__btn');
+
+  resultBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    displayNumber = solution();
     dispalyUpdate();
   })
 }
 
-//отсановился на решении проблемы с "="
+//ехе хех ехх хе хе ОН работает)
 
-readNumber()
-clearDisplay()
-result();
+//далее наверное оптимизация, также надо сделать так чтобы после вывода решения, когда новое число начинаешь вводить, все стиралось для начала.
+//Можно реализовать дальше 
+//1) Степени
+//2) М+ и М- 
+//3) удаление посимвольно
+//4) ограничение по количетсву символов
+//5) плавающая точка
+
+readNumber();
+saveValue();
+resultOutput()
+clearDisplayOnClick();
 dispalyUpdate();
